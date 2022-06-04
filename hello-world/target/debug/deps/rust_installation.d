4feb54e882b98caf51944f22d9f1363801fa4b74
Oyewo Temidayo <oyewodayo@gmail.com>
2022-06-04 09:34:13 +0100

    First rust project. Hello world!

---
+++ hello-world/target/debug/deps/rust_installation.d
@@ -0,0 +1,5 @@
+C:\Users\HP\Desktop\pathtorust\rust-installation\target\debug\deps\rust_installation.exe: src\main.rs
+
+C:\Users\HP\Desktop\pathtorust\rust-installation\target\debug\deps\rust_installation.d: src\main.rs
+
+src\main.rs:
---
+++ hello-world/target/debug/rust-installation.d
@@ -0,0 +1,1 @@
+C:\Users\HP\Desktop\pathtorust\hello-world\target\debug\rust-installation.exe: C:\Users\HP\Desktop\pathtorust\hello-world\src\main.rs
